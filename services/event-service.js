732087bcdc201d30376f94eb2c2d24dda668ea6b
Liu, Mason <a483848@fmr.com>
2018-05-07 15:46:11 +0800

    event

--- services/event-service.js
+++ services/event-service.js
@@ -3,11 +3,8 @@ var messageSendService = require('./message-send-service');
 
 module.exports = {
     processEvent: function(event, app) {
-        if(msgtype === 'event' && event.event === 'SCAN') {
-            scanService.processScan(event, app);
-        }
-        if(msgtype === 'text') {
-            return messageSendService.replayMessage()
+        if(event.msgtype === 'event' && event.event === 'SCAN') {
+            return scanService.processScan(event, app);
         }
     }
 }
