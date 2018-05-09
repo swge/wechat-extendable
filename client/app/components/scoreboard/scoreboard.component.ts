02db75c1a2cc5fa0d87eca1947c48cce5697d514
Shen, Esther <Xingtong.Shen@fmr.com>
2018-05-09 15:37:11 +0800

    remove mock

--- client/app/components/scoreboard/scoreboard.component.ts
+++ client/app/components/scoreboard/scoreboard.component.ts
@@ -13,41 +13,5 @@ export class ScoreboardComponent implements OnInit {
 
     ngOnInit() {
         this.ttfService.getFinalScore();
-        this.ttfService.finalScore = [
-            {
-                avatar: 'http://thirdwx.qlogo.cn/mmopen/m2BRRRIDoJqxuMu8Ec3gtHRxbKicbhkrTw3kic9mp7GzGH48PAkGbk87hAvA3yh3U6NNXho1OA1M8nicAqxzqJ3ogtt9gZQJEyp/132',
-                name: 'Jerry Wang',
-                score: 5
-            },
-            {
-                avatar: 'http://thirdwx.qlogo.cn/mmopen/m2BRRRIDoJqxuMu8Ec3gtHRxbKicbhkrTw3kic9mp7GzGH48PAkGbk87hAvA3yh3U6NNXho1OA1M8nicAqxzqJ3ogtt9gZQJEyp/132',
-                name: 'Jerry Wang',
-                score: 5
-            },
-            {
-                avatar: 'http://thirdwx.qlogo.cn/mmopen/m2BRRRIDoJqxuMu8Ec3gtHRxbKicbhkrTw3kic9mp7GzGH48PAkGbk87hAvA3yh3U6NNXho1OA1M8nicAqxzqJ3ogtt9gZQJEyp/132',
-                name: 'Jerry Wang',
-                score: 5
-            },
-            {
-                avatar: 'http://thirdwx.qlogo.cn/mmopen/m2BRRRIDoJqxuMu8Ec3gtHRxbKicbhkrTw3kic9mp7GzGH48PAkGbk87hAvA3yh3U6NNXho1OA1M8nicAqxzqJ3ogtt9gZQJEyp/132',
-                name: 'Jerry Wang',
-                score: 5
-            },
-            {
-                avatar: 'http://thirdwx.qlogo.cn/mmopen/m2BRRRIDoJqxuMu8Ec3gtHRxbKicbhkrTw3kic9mp7GzGH48PAkGbk87hAvA3yh3U6NNXho1OA1M8nicAqxzqJ3ogtt9gZQJEyp/132',
-                name: 'Jerry Wang',
-                score: 5
-            },
-            {
-                avatar: 'http://thirdwx.qlogo.cn/mmopen/m2BRRRIDoJqxuMu8Ec3gtHRxbKicbhkrTw3kic9mp7GzGH48PAkGbk87hAvA3yh3U6NNXho1OA1M8nicAqxzqJ3ogtt9gZQJEyp/132',
-                name: 'Jerry Wang',
-                score: 5
-            },
-            {
-                avatar: 'http://thirdwx.qlogo.cn/mmopen/m2BRRRIDoJqxuMu8Ec3gtHRxbKicbhkrTw3kic9mp7GzGH48PAkGbk87hAvA3yh3U6NNXho1OA1M8nicAqxzqJ3ogtt9gZQJEyp/132',
-                name: 'Jerry Wang',
-                score: 5
-            }];
     }
 }
