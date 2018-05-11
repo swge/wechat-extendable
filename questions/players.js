89465d7d79569ce99ea9c8b62f8c979ee36d5dec
Shen, Esther <Xingtong.Shen@fmr.com>
2018-05-11 13:50:13 +0800

    add stone pic

--- questions/players.js
+++ questions/players.js
@@ -52,26 +52,13 @@ module.exports = [
         wrongAnswers: {}
     },
     {
-        name: 'Jerry Wang',
-        nickName: '一叶知秋',
-        image: '/stgw/digital/wechat/dist/assets/bg-story.png',
+        name: 'Stone Shi',
+        nickName: '石头',
+        image: '/stgw/digital/wechat/dist/assets/stone.jpg',
         questions: [
             {'label': 'a', 'content': 'I have a 8 years old daughter', 'correct': true},
             {'label': 'b', 'content': 'I have a 9 years old daughter', 'correct': false},
             {'label': 'c', 'content': 'I have a 10 years old daughter', 'correct': false}
-        ],
-        correctQuestion: 'a',
-        rightAnswers: {},
-        wrongAnswers: {}
-    },
-    {
-        name: 'Jenny Liu',
-        nickName: 'Jenny',
-        image: '/stgw/digital/wechat/dist/assets/bg-story.png',
-        questions: [
-            {'label': 'a', 'content': 'I have a 3 years old son', 'correct': true},
-            {'label': 'b', 'content': 'I have a 4 years old son', 'correct': false},
-            {'label': 'c', 'content': 'I have a 5 years old son', 'correct': false}
         ],
         correctQuestion: 'a',
         rightAnswers: {},
