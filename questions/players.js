e1515f76e9a2380d8019109ef05017c47c40880a
Liu, Mason <a483848@fmr.com>
2018-05-18 15:30:21 +0800

    tim

--- questions/players.js
+++ questions/players.js
@@ -26,19 +26,6 @@ module.exports = [
         wrongAnswers: {}
     },
     {
-        name: 'Tim Wang',
-        nickName: '王胖子',
-        image: '/stgw/digital/wechat/dist/assets/Tim.jpg',
-        questions: [
-            {'label': 'a', 'content': '我曾经是年级的球队守门员，并且在最后一场比赛中只用脚扑出了三个点球赢得了比赛。', 'correct': true},
-            {'label': 'b', 'content': '我在星海广场扮成轻松熊跟我媳妇求婚并上了半岛晨报的新闻。', 'correct': true},
-            {'label': 'c', 'content': '我参加过一次两天48小时不间断的的游戏开发大赛，并拿到了名次。', 'correct': false}
-        ],
-        correctQuestion: 'c',
-        rightAnswers: {},
-        wrongAnswers: {}
-    },
-    {
         name: 'Ivan Wang',
         nickName: '小黄人',
         image: '/stgw/digital/wechat/dist/assets/Ivan.jpg',
@@ -59,6 +46,19 @@ module.exports = [
             {'label': 'a', 'content': '我曾经在毫无征兆的情况下撞上过电线杆，结果伤痕累累。', 'correct': true},
             {'label': 'b', 'content': '我曾经被鱼咬过一大口，并且咬破了。', 'correct': true},
             {'label': 'c', 'content': '我曾经误把我爸当成坏人，并向警察叔叔（小区保安）寻求帮助', 'correct': false}
+        ],
+        correctQuestion: 'c',
+        rightAnswers: {},
+        wrongAnswers: {}
+    },
+    {
+        name: 'Tim Wang',
+        nickName: '王胖子',
+        image: '/stgw/digital/wechat/dist/assets/Tim.jpg',
+        questions: [
+            {'label': 'a', 'content': '我曾经是年级的球队守门员，并且在最后一场比赛中只用脚扑出了三个点球赢得了比赛。', 'correct': true},
+            {'label': 'b', 'content': '我在星海广场扮成轻松熊跟我媳妇求婚并上了半岛晨报的新闻。', 'correct': true},
+            {'label': 'c', 'content': '我参加过一次两天48小时不间断的的游戏开发大赛，并拿到了名次。', 'correct': false}
         ],
         correctQuestion: 'c',
         rightAnswers: {},
