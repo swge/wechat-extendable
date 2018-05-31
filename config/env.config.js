ac71e91ee5f577ea427d8a6d7885f61236be3e61
Mason Liu <meteorainl@163.com>
2018-05-31 22:05:46 +0800

    use DalianPIT

--- config/env.config.js
+++ config/env.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     STGW_URL: process.env.STGW_URL || '/stgw/digital/wechat',
     PRGW_URL: process.env.STGW_URL || '/prgw/digital/wechat',
-    APP_ID: process.env.APP_ID || 'wxae3fdfd23c057214',
-    APP_SECRET: process.env.APP_SECRET || '4bfc03494f7d4057d4a800e492c71ca4',
+    APP_ID: process.env.APP_ID || 'wx54bb38780fd3c7c1',
+    APP_SECRET: process.env.APP_SECRET || 'f9942bcc5e17cce0dc6b44b8534f22bb',
     TOKEN: process.env.TOKEN || 'DalianPIT'
 }
