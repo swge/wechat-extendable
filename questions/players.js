11870635942831b1796c60bef6e5b39a7224bd8a
Shen, Esther <Xingtong.Shen@fmr.com>
2018-06-01 14:20:27 +0800

    round4

--- questions/players.js
+++ questions/players.js
@@ -1,66 +1,66 @@
 module.exports = [
     {
-        name: 'Gavin Pang',
-        nickName: 'Mr. Pang',
-        image: '/stgw/digital/wechat/dist/assets/gavin.jpg',
+        name: 'Ammy Li',
+        nickName: 'Ammy',
+        image: '/stgw/digital/wechat/dist/assets/ammy.jpg',
         questions: [
-            {'label': 'a', 'content': 'Volleyball', 'correct': false},
-            {'label': 'b', 'content': 'Groomsman', 'correct': true},
-            {'label': 'c', 'content': 'Roommate', 'correct': true}
+            {'label': 'a', 'content': '桌式足球比赛', 'correct': false},
+            {'label': 'b', 'content': '撞电线杆', 'correct': true},
+            {'label': 'c', 'content': '曾经养过一只小泰迪', 'correct': true}
         ],
         correctQuestion: 'a',
         rightAnswers: {},
         wrongAnswers: {}
     },
     {
-        name: 'Shell Wang',
-        nickName: '澍',
-        image: '/stgw/digital/wechat/dist/assets/shell.jpg',
+        name: 'Elaine He',
+        nickName: 'Elaine',
+        image: '/stgw/digital/wechat/dist/assets/elaine.jpg',
         questions: [
-            {'label': 'a', 'content': 'naughty', 'correct': false},
-            {'label': 'b', 'content': 'Ran away to home', 'correct': true},
-            {'label': 'c', 'content': 'Car', 'correct': true}
+            {'label': 'a', 'content': '我养了一个小狗，但是每次吃饭的时候会发很大很大很大的脾气。一边生气一边吃', 'correct': false},
+            {'label': 'b', 'content': '去泰国普吉岛的时候，因为我不能下水，爸爸为了体现父爱空手去拿海胆 结果感染得了一场大病', 'correct': true},
+            {'label': 'c', 'content': '我练了几年的瑜伽 ，现在可以倒立了', 'correct': true}
         ],
         correctQuestion: 'a',
         rightAnswers: {},
         wrongAnswers: {}
     },
     {
-        name: 'Eric Li',
-        nickName: '小项目之王',
-        image: '/stgw/digital/wechat/dist/assets/xiannan.jpg',
+        name: 'Frank Feng',
+        nickName: 'Frank',
+        image: '/stgw/digital/wechat/dist/assets/frank.jpg',
         questions: [
-            {'label': 'a', 'content': '奥运火炬手', 'correct': false},
-            {'label': 'b', 'content': '握手', 'correct': true},
-            {'label': 'c', 'content': '逆行', 'correct': true}
+            {'label': 'a', 'content': '浮潜', 'correct': true},
+            {'label': 'b', 'content': '旅顺长大', 'correct': true},
+            {'label': 'c', 'content': '广州旅游', 'correct': false}
         ],
-        correctQuestion: 'a',
+        correctQuestion: 'c',
         rightAnswers: {},
         wrongAnswers: {}
     },
     {
-        name: 'Bill Mao',
-        nickName: '型男18岁',
-        image: '/stgw/digital/wechat/dist/assets/bill.jpg',
+        name: 'Michelle Zhang',
+        nickName: '小红',
+        image: '/stgw/digital/wechat/dist/assets/michelle.jpg',
         questions: [
-            {'label': 'a', 'content':'大二时体重不足120斤，毕业那年体重达到人生巅峰180斤。', 'correct': true},
-            {'label': 'b', 'content': '作为骑行北理的成员之一，我在大一到大二暑假一年间内，跟队或带队骑行了G102北京-哈尔滨、G110北京-包头、G318重庆-成都-拉萨三条长途，总路程近5000km。', 'correct': false},
-            {'label': 'c', 'content': '大学毕业后应招北京某空军部队入伍，后因每天需要跑早操最终放弃。', 'correct': true}
+            {'label': 'a', 'content':'I killed a dog, cruelly', 'correct': true},
+            {'label': 'b', 'content': 'Me, piano and band', 'correct': false},
+            {'label': 'c', 'content': '一站式购物', 'correct': true}
         ],
         correctQuestion: 'b',
         rightAnswers: {},
         wrongAnswers: {}
     },
     {
-        name: 'Evan Xu',
-        nickName: '饭长',
-        image: '/stgw/digital/wechat/dist/assets/evan.jpg',
+        name: 'Kevin Ge',
+        nickName: 'KG',
+        image: '/stgw/digital/wechat/dist/assets/kevin.jpg',
         questions: [
-            {'label': 'a', 'content': 'CABIN', 'correct': true},
-            {'label': 'b', 'content': 'ROOF', 'correct': true},
-            {'label': 'c', 'content': 'KAYAK', 'correct': false}
+            {'label': 'a', 'content': '虾片', 'correct': false},
+            {'label': 'b', 'content': '严肃脸', 'correct': true},
+            {'label': 'c', 'content': '考大学', 'correct': true}
         ],
-        correctQuestion: 'c',
+        correctQuestion: 'a',
         rightAnswers: {},
         wrongAnswers: {}
     }
