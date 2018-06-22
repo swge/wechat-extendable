90c1564b6f4760ddbfb00e1b26f40f4912fbef45
swge <Shiwen.Keh@gmail.com>
2018-06-22 13:43:10 +0800

    round 7

--- questions/players.js
+++ questions/players.js
@@ -1,66 +1,66 @@
 module.exports = [
     {
-        name: 'Susan Sun',
-        nickName: 'Susan',
-        image: '/stgw/digital/wechat/dist/assets/Susan.jpg',
+        name: 'Barry Xue',
+        nickName: 'Barry',
+        image: '/stgw/digital/wechat/dist/assets/Barry.jpg',
         questions: [
-            {'label': 'a', 'content': '水缸', 'correct': true},
-            {'label': 'b', 'content': '数学老师', 'correct': false},
-            {'label': 'c', 'content': '汉语拼音', 'correct': true}
-        ],
-        correctQuestion: 'b',
-        rightAnswers: {},
-        wrongAnswers: {}
-    },
-    {
-        name: 'Nicole Zhang',
-        nickName: 'Nicole',
-        image: '/stgw/digital/wechat/dist/assets/Nicole.png',
-        questions: [
-            {'label': 'a', 'content': '迷路', 'correct': true},
-            {'label': 'b', 'content': '诈骗', 'correct': false},
-            {'label': 'c', 'content': '晕倒', 'correct': true}
-        ],
-        correctQuestion: 'b',
-        rightAnswers: {},
-        wrongAnswers: {}
-    },
-    {
-        name: 'Neo Wang',
-        nickName: 'Neo',
-        image: '/stgw/digital/wechat/dist/assets/Neo.jpg',
-        questions: [
-            {'label': 'a', 'content': '走回家', 'correct': false},
-            {'label': 'b', 'content': '减肥', 'correct': true},
-            {'label': 'c', 'content': '输入法', 'correct': true}
-        ],
-        correctQuestion: 'a',
-        rightAnswers: {},
-        wrongAnswers: {}
-    },
-    {
-        name: 'Francis Li',
-        nickName: 'Francis',
-        image: '/stgw/digital/wechat/dist/assets/Francis.jpg',
-        questions: [
-            {'label': 'a', 'content':'Safe', 'correct': true},
-            {'label': 'b', 'content': 'Newspaper', 'correct': true},
-            {'label': 'c', 'content': 'Dating', 'correct': false}
+            {'label': 'a', 'content': 'hobby: ping pang sport', 'correct': true},
+            {'label': 'b', 'content': 'have a daughter with 8 years old', 'correct': true},
+            {'label': 'c', 'content': 'like small animals and have a pet (chihuahua dog)', 'correct': false}
         ],
         correctQuestion: 'c',
         rightAnswers: {},
         wrongAnswers: {}
     },
     {
-        name: 'Jackiy Huang',
-        nickName: 'Jackiy',
-        image: '/stgw/digital/wechat/dist/assets/Jackiy.jpg',
+        name: 'Linda Wang',
+        nickName: 'Linda',
+        image: '/stgw/digital/wechat/dist/assets/Linda.jpg',
         questions: [
-            {'label': 'a', 'content': '9毛', 'correct': true},
-            {'label': 'b', 'content': '鼠标', 'correct': false},
-            {'label': 'c', 'content': '家教', 'correct': true}
+            {'label': 'a', 'content': '毕业后最穷一天', 'correct': false},
+            {'label': 'b', 'content': '学科专业以及职业的选择', 'correct': true},
+            {'label': 'c', 'content': '唯一一次挂科经历', 'correct': true}
+        ],
+        correctQuestion: 'a',
+        rightAnswers: {},
+        wrongAnswers: {}
+    },
+    {
+        name: 'Ally Xu',
+        nickName: 'Ally',
+        image: '/stgw/digital/wechat/dist/assets/Ally.jpg',
+        questions: [
+            {'label': 'a', 'content': '学会游泳', 'correct': false},
+            {'label': 'b', 'content': '七年之痒', 'correct': true},
+            {'label': 'c', 'content': '留长发', 'correct': true}
+        ],
+        correctQuestion: 'a',
+        rightAnswers: {},
+        wrongAnswers: {}
+    },
+    {
+        name: 'Nylon Gao',
+        nickName: 'Nylon',
+        image: '/stgw/digital/wechat/dist/assets/Nylon.jpg',
+        questions: [
+            {'label': 'a', 'content':'Brick', 'correct': true},
+            {'label': 'b', 'content': 'Love', 'correct': false},
+            {'label': 'c', 'content': 'King', 'correct': true}
         ],
         correctQuestion: 'b',
+        rightAnswers: {},
+        wrongAnswers: {}
+    },
+    {
+        name: 'Effy Zhang',
+        nickName: 'Effy',
+        image: '/stgw/digital/wechat/dist/assets/person-default.png',
+        questions: [
+            {'label': 'a', 'content': '甜甜圈', 'correct': true},
+            {'label': 'b', 'content': '猪队友', 'correct': true},
+            {'label': 'c', 'content': '螳螂捕蝉黄雀在后', 'correct': false}
+        ],
+        correctQuestion: 'c',
         rightAnswers: {},
         wrongAnswers: {}
     }
