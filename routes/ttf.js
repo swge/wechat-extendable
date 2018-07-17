770bc7951007cc765b749d9d58fdc79429c0ad50
Evan.Xu <Evan.Xu@fmr.com>
2018-07-17 16:42:41 +0800

    move save method to round winners

--- routes/ttf.js
+++ routes/ttf.js
@@ -6,6 +6,19 @@ var envConfig = require('../config/env.config');
 router.get('/round-winners', function (req, res, next) {
     req.app.questionGameData.active = false;
     if(req.app.currentQuestionPlayer) {
+        const jsonPath = path.join(__dirname, '..', 'questions', 'players.json');
+        const jsonString = fs.readFileSync(jsonPath, 'utf8');
+        const allPlayers = JSON.parse(jsonString);
+        const currentQuestionPlayer = req.app.currentQuestionPlayer;
+        console.log('app.currentQuestionPlayer: ' + app.currentQuestionPlayer);
+        for (const iter of allPlayers) {
+            if (iter.name === currentQuestionPlayer.name && iter.nickName === currentQuestionPlayer.nickName) {
+                iter.rightAnswers = {...iter.rightAnswers, ...currentQuestionPlayer.rightAnswers};
+                iter.wrongAnswers = {...iter.wrongAnswers, ...currentQuestionPlayer.wrongAnswers};
+                break;                
+            }
+        }
+        fs.writeFileSync(jsonPath, JSON.stringify(allPlayers), 'utf8');
         res.json(Object.values(req.app.currentQuestionPlayer.rightAnswers));
     }
 });
